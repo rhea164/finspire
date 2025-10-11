d5255c8157c0d81ebc243a93b7f3e6a6d85d8570
Tamara Coelho <107127725+tamaracoelho27@users.noreply.github.com>
2025-10-11 13:12:59 +0400

    Add files via upload

---
+++ frontend/landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './landing.css';
+
+function Landing(){
+    return (
+     <div>
+     <h1 className="Topic">Choose Your Topic</h1>
+     </div>
+    )
+}
+
+export default Landing;
