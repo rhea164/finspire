8b38e7ff9004a2c080bd80f2d64abe00e85f4858
Tamara Coelho <107127725+tamaracoelho27@users.noreply.github.com>
2025-10-11 15:25:10 +0400

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
-import './App.css';
-import Landing from './components/landing'
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Landing from './components/Landing';
+import Avatar from './components/Avatar';
 
 function App() {
   return (
-    <div>
-      <Landing></Landing>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/avatar" element={<Avatar />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+export default App;
