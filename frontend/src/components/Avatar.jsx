20bcbbbd55b99d299c2c6f72172bf8a51b634f7c
Tamara Coelho <107127725+tamaracoelho27@users.noreply.github.com>
2025-10-11 20:47:24 +0400

    Update Avatar.jsx

--- frontend/src/components/Avatar.jsx
+++ frontend/src/components/Avatar.jsx
@@ -1,5 +1,5 @@
 // components/Avatar.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import './Avatar.css';
 import zeeshan from '../assets/zeeshan.png';
 import rhea from '../assets/rhea.png';
@@ -9,22 +9,33 @@ import lak from '../assets/lak.png'
 import vansh from '../assets/vansh.png'
 
 function Avatar() {
+  const [selectedAvatar, setSelectedAvatar] = useState(null);
+
+  const avatars = [
+    { src: zeeshan, className: 'z' },
+    { src: rhea, className: 'r' },
+    { src: kanch, className: 'k' },
+    { src: tams, className: 't' },
+    { src: lak, className: 'l' },
+    { src: vansh, className: 'v' }
+  ];
 
   return (
     <div className="avatar-container">
       <h1 className="avatar">SELECT YOUR AVATAR</h1>
-
-      <div className="avatars">
-        <img className="z"src={zeeshan}></img>
-        <img className="r"src={rhea}></img>
-        <img className="k"src={kanch}></img>
-        <img className="t" src={tams}></img>
-        <img className="l"src={lak}></img>
-        <img className="v"src={vansh}></img>
-      </div>
-    <button>Confirm</button>
-    </div>
-  );
+       <div className="avatars">
+        {avatars.map((avatar, index) => (
+          <img 
+            key={index}
+            className={`${avatar.className} ${selectedAvatar === index ? 'selected' : ''}`}
+            src={avatar.src}
+            onClick={() => setSelectedAvatar(index)}
+          ></img>
+        ))}
+       </div>
+     <button>Confirm</button>
+     </div>
+   );
 }
 
 export default Avatar;
