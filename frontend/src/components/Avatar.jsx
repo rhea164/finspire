e854de3ba5b061a10ec75aed11f28f42ebb5ce40
Tamara Coelho <107127725+tamaracoelho27@users.noreply.github.com>
2025-10-11 16:44:05 +0400

    Update Avatar.jsx

--- frontend/src/components/Avatar.jsx
+++ frontend/src/components/Avatar.jsx
@@ -1,11 +1,29 @@
 // components/Avatar.jsx
 import React from 'react';
 import './Avatar.css';
+import zeeshan from '../assets/zeeshan.png';
+import rhea from '../assets/rhea.png';
+import kanch from '../assets/kanch.png';
+import tams from '../assets/tams.png'
+import lak from '../assets/lak.png'
+import vansh from '../assets/vansh.png'
 
 function Avatar() {
 
   return (
-    <h1 className="avatar">SELECT YOUR AVATAR</h1>
+    <div className="avatar-container">
+      <h1 className="avatar">SELECT YOUR AVATAR</h1>
+
+      <div className="avatars">
+        <img className="z"src={zeeshan}></img>
+        <img className="r"src={rhea}></img>
+        <img className="k"src={kanch}></img>
+        <img className="t" src={tams}></img>
+        <img className="l"src={lak}></img>
+        <img className="v"src={vansh}></img>
+      </div>
+    <button>Confirm</button>
+    </div>
   );
 }
 
